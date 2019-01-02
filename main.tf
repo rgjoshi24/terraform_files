ae19dfd04fa682b16bba7a10fb4a6da5f567dc4e
root <root@rgjoshi241.mylabserver.com>
2019-01-02 21:42:12 +0000

    Added variables

--- main.tf
+++ main.tf
@@ -1,15 +1,35 @@
 #download latest ghost image
 
+variable "image_name" {
+  description = "Name of the container image"
+  default = "ghost:latest"
+}
+
+variable "container_name" {
+  description = "Name of the container"
+  default = "blod"
+}
+
+variable "int_port" {
+  description = "Internal Port Number"
+  default = "2368"
+}
+
+variable "ext_port" {
+  description = "Internal Port Number"
+  default = "80"
+}
+
 resource "docker_image" "image_id" {
-  name = "ghost:latest"
+  name = "${var.image_name}"
 }
 
 resource "docker_container" "container_id" {
-  name = "blog"
+  name = "${var.container_name}"
   image = "${docker_image.image_id.latest}"
   ports {
-    internal = "2368"
-    external = "80"
+    internal = "${var.int_port}"
+    external = "${var.ext_port}"
   }
 }
 
